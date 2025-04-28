541d27eb2201a2a6c086ade10706b1169431ff79
Gaurav Sharma <2gauravpandit@gmail.com>
2025-04-28 13:11:17 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// #include<conio.h>
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Number = %d", a);
+    // printf("%d", a);
+    return 0;
+};
